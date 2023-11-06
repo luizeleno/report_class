58014f99b5f55216335ba63520ce0757241e84e0
Luiz T. F. Eleno <luizeleno@gmail.com>
2023-11-06 16:41:08 -0300

    apagadas cópias no docs

--- teste.py
+++
@@ -1,6 +0,0 @@
-import src.report_class as rc
-
-dados = rc.turma(planilha='Notas.xls', sr=5, disciplina='LOM3226', ano=2023, semestre=1)
-dados.create_cabecalho('Resultado Final')
-dados.create_table('P1', 'P2', 'NF')
-dados.create_latex_report(['P1', 'P2'], runlatex=True, args=['papersize={220mm, 200mm}'])
